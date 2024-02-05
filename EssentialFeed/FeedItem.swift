6ec2ac70238e22d9fb49d8e0337102fdbcd81b01
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2024-02-05 17:33:33 -0300

    feat: create FeedItem Model to be used to comunicate between modules

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Matheus Gomes on 05/02/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+    
+}
