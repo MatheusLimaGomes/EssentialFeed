08df95209ed29e53b671186e59f72ae5af799e45
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2024-02-06 16:39:42 -0300

    feat: Delivers connectivity error client error

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -28,6 +28,13 @@ final class RemoteFeedLoaderTests: XCTestCase {
         sut.load()
         XCTAssertEqual(client.requestedURLs, [url,url])
     }
+    func test_load_deliversErroOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        var capturedError: RemoteFeedLoader.Error?
+        sut.load { error in capturedError = error }
+        XCTAssertEqual(capturedError, .connectivity)
+    }
 }
 
 // MARK: - Helpers
@@ -40,7 +47,11 @@ extension RemoteFeedLoaderTests {
     
     private class HTTPClientSPy: HTTPClient {
         var requestedURLs = [URL]()
-        func get(from url: URL) {
+        var error: Error?
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
