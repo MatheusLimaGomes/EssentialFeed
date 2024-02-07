6db7ad09e9c09ee32d2191c154495687df427382
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2024-02-07 11:02:20 -0300

    feat: Replace params with enum with success/failure cases

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -64,17 +64,17 @@ extension RemoteFeedLoaderTests {
         return (sut, client)
     }
     private class HTTPClientSPy: HTTPClient {
-        private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
         var requestedURLs: [URL] {
             return messages.map {$0.url}
         }
         var completions = [(Error) -> Void]()
         
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url,completion))
         }
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error  ))
         }
         func complete(withStatusCode code: Int, at index: Int = 0) {
             let response = HTTPURLResponse(
@@ -82,8 +82,8 @@ extension RemoteFeedLoaderTests {
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
-            )
-            messages[index].completion(nil, response)
+            )!
+            messages[index].completion(.success(response))
         }
     }
 }
