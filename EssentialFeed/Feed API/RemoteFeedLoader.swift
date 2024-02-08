b6bc391fbb8c4cdd73c7ac21b723883d6c8a1f40
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2024-02-08 10:57:54 -0300

    feat: move mapping logic to a new type `FeedItemsMapper`.

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -37,9 +37,10 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try?   JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map({$0.item})))
-                } else {
+                do {
+                    let items = try FeedItemsMapper.map(data, response: response)
+                    completion(.success(items))
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure:
@@ -49,21 +50,26 @@ public final class RemoteFeedLoader {
     }
 }
 
-
-private struct Root: Decodable {
-    let items: [Item]
-}
-
-private struct Item: Decodable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let image: URL
-    
-    var item: FeedItem {
-        return FeedItem(
-            id: id, description: description,
-            location: location, imageURL: image
-        )
+private class FeedItemsMapper {
+    struct Root: Decodable {
+        let items: [Item]
+    }
+    struct Item: Decodable {
+        public let id: UUID
+        public let description: String?
+        public let location: String?
+        public let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(
+                id: id, description: description,
+                location: location, imageURL: image
+            )
+        }
+    }
+    static func map(_ data: Data, response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else { throw RemoteFeedLoader.Error.invalidData }
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map({$0.item})
     }
 }
