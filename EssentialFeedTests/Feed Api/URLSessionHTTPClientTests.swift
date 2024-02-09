f16b631cb03065fe3a4f029db354a299b9f19638
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2024-02-09 16:10:08 -0300

    refact: Move the URLSessionHTTPClient (the system under test, or "SUT") creation to a factory method to protect our tests from breaking changes

--- EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -31,7 +31,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.stopInterceptingRequests()
     }
     func test_getFromURL_performGetRequestWithURL() {
-        let sut = URLSessionHTTPClient()
         let url = URL(string: "http://any-url.com")!
         
         let exp = expectation(description: "Wait for request")
@@ -40,7 +39,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             XCTAssertEqual(request.httpMethod, "GET")
             exp.fulfill()
         }
-        sut.get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         wait(for: [exp], timeout: 1.0)
     }
     func test_getFromURL_failsOnRequestError() {
@@ -48,11 +47,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
-        let sut = URLSessionHTTPClient()
-        
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertNotNil(receivedError)
@@ -62,6 +59,9 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
+    }
+    private func makeSUT() -> URLSessionHTTPClient {
+        URLSessionHTTPClient()
     }
 
     private class URLProtocolStub: URLProtocol {
