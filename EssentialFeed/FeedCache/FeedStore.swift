4b0be44f9c246416f186794b8d697fcbf8c9870a
Matheus Francisco da Silva Lima Gomes <43860420+MatheusLimaGomes@users.noreply.github.com>
2024-02-21 22:21:20 -0300

    Implements FeedCache Use case (#9)
    * doc: remove deprecated pipeline logo
    
    * feat: Does not delete cache upon `LocalFEedLoader`creation.
    
    * feat: save command requests cache deletion
    
    * refact: extract system under test creation to a factory method to protect tests against breaking changes.
    
    * Move memorry leak tracking extension from the FeedApi Module scope to a module level shared scope. The HTTPClientSpy stil into the API module scope.
    
    * feat: add memory leak tracking
    
    * feat: Save command does not request cache insertion on deletion error
    
    * feat: Save command requests new cache insertion on successful deletion
    
    * feat: Save command requests cache insertion with timestamp on successful deletion
    
    * chore: Remove redundant test code
    
    * chore: Unify store helper received messages to guarantee order and simplify test assertions
    
    * feat: Save command fails on cache deletion error
    
    * feat: Save command fails on cache insertion error
    
    * feat: Save command fails on cache insertion error
    
    * feat: Save command succeeds on successfull cache insertion
    
    * chore: Extract duplicated test code into a shared helper method
    
    * refact: Extract `FeedStore` protocol from test spy helper
    
    * feat: Guarantee the `LocalFeedLoader` does not deliver deletion error after instance has been deallocated
    
    * refact: remove unused code
    
    * refact: Guarante that the `LocalFeedLoader` doesn't deliver insertion error, after instance has been deallocated
    
    * refact: Invert if logic to make code paths easier to follow
    
    * refact: Extract cache insertion into a helper function to make logic inside closure callbacks easier to follow
    
    * refact: Move `LocalFeedLoader` and `FeedStore` collaborator to it own file in produciton.
    
    * refact: Move feed store to its own file.
    
    * refact: Add `SaveResult` typealias to protect portential breaking changes
    
    * chore: Remove unused `Essential Feed` documentation from the system. We are using the Readme to guide our documentation code.

---
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,15 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Matheus Gomes on 21/02/24.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
