2f3413c0c2fe14fc93b20e726a6ee58795c7ab96
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2024-02-28 12:00:29 -0300

    feat: Validate cache command does not delete cache when cache is already emty

--- EssentialFeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
@@ -24,6 +24,16 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
     }
     
+    
+    func test_validateCache_doesNotDeletesCacheOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        
+        sut.validateCache() 
+        store.completeRetrievalWithEmptyCache()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: - HELPERS
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
