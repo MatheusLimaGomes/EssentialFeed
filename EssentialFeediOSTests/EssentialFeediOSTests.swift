c0e03e5836e8425fdc6b876f05211c3624b7c6df
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2024-05-14 11:42:28 -0300

    Add 'EssentialFeediOS'framework (prod and test) targets for iOS platform-specific components

---
+++ EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeediOSTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Matheus Gomes on 14/05/24.
+//
+
+import XCTest
+
+final class EssentialFeediOSTests: XCTestCase {
+
+}
