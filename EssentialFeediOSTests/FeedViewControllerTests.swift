622f3b27229f5f5a377b376d1a595a53973a2ef8
MatheusLimaGomes <matheus.lima.gomes@outlook.com>
2024-05-17 19:51:20 -0300

    Test the load view and refresh control behaviour, using a helper method to simulate the appearance.

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -20,11 +20,16 @@ final class FeedViewController: UITableViewController {
         super.viewDidLoad()
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        refreshControl?.beginRefreshing()
+    }
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
         load()
     }
     @objc private func load() {
-        loader?.load{ _ in }
+        refreshControl?.beginRefreshing()
+        loader?.load{ [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
     }
 }
 final class FeedViewControllerTests: XCTestCase {
@@ -36,26 +41,37 @@ final class FeedViewControllerTests: XCTestCase {
     func test_viewDidLoad_loadsFeed() {
         let (sut, loader) = makeSUT()
         
-        sut.loadViewIfNeeded()
+        sut.simulateAppearance()
+        loader.completeFeedLoading()
         
         XCTAssertEqual(loader.loaderCallCount, 1)
-    }
-    func test_pullToRefresh_loadsFeed() {
-        let (sut, loader) = makeSUT()
-        sut.loadViewIfNeeded()
         
         sut.refreshControl?.simulatePullToRefresh()
+        loader.completeFeedLoading()
+        
         XCTAssertEqual(loader.loaderCallCount, 2)
         
         sut.refreshControl?.simulatePullToRefresh()
+        loader.completeFeedLoading()
+        
         XCTAssertEqual(loader.loaderCallCount, 3)
     }
     func test_viewDidLoad_showsLoadingIndicator() {
         let (sut, _) = makeSUT()
         
-        sut.loadViewIfNeeded()
+        sut.simulateAppearance()
+        
         XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
     }
+    func test_viewDidLoad_hidesLoadingIndicatorOnLoaderCompletion() {
+        let (sut, loader) = makeSUT()
+        
+        sut.simulateAppearance()
+        loader.completeFeedLoading()
+        
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+    }
+    
     // MARK: - Helpers
     private func makeSUT(
         file: StaticString = #filePath,
@@ -67,22 +83,38 @@ final class FeedViewControllerTests: XCTestCase {
         trackForMemoryLeaks(instance: sut, file: file, line: line)
         return (sut, loader)
     }
-}
-class LoaderSpy: FeedLoader {
-    private(set) var loaderCallCount: Int = 0
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        loaderCallCount += 1
+    private class LoaderSpy: FeedLoader {
+        private var completions = [(FeedLoader.Result) -> Void]()
+        var loaderCallCount: Int {
+            return completions.count
+        }
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            completions.append(completion)
+        }
+        func completeFeedLoading() {
+            completions[0](.success([]))
+        }
     }
 }
-private extension UIRefreshControl {
-    func simulatePullToRefresh() {
-        allTargets.forEach { target in
-            actions(
+private extension FeedViewController {
+    func simulateAppearance() {
+        if !isViewLoaded {
+            loadViewIfNeeded()
+            replaceRefreshControlWithFakeForiOS17Support()
+        }
+        beginAppearanceTransition(true, animated: false)
+        endAppearanceTransition()
+    }
+    func replaceRefreshControlWithFakeForiOS17Support() {
+        let fake = FakeRefreshControl()
+        refreshControl?.allTargets.forEach{ target in
+            refreshControl?.actions(
                 forTarget: target,
                 forControlEvent: .valueChanged
-            )?.forEach {
-                (target as NSObject).perform(Selector($0))
+            )?.forEach { action in
+                fake.addTarget(target, action: Selector(action), for: .valueChanged)
             }
         }
+        refreshControl = fake
     }
 }
